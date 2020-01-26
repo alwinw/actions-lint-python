d15c3716319342f9e02147b17f4d81a01ac517b5
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-27 09:05:49 +1100

    Testing of entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+
+function pretty_print() {
+    if [[ $2 -eq -1 ]]
+    then
+        echo -e "\n"
+        local MESSAGE="Running $1 Test"
+        local COLOUR=4
+    elif [[ $2 -eq 0 ]]; then
+        local MESSAGE="$1 Test Passed!"
+        local COLOUR=2
+    elif [[ $2 -eq 1 ]]; then
+        local MESSAGE="$1 Test Failed!"
+        local COLOUR=1
+    else
+        echo -e "\n"
+        local MESSAGE="$1"
+        local COLOUR=4
+    fi
+    echo -e "\e[3${COLOUR}m##########\e[39m     \e[4${COLOUR}m ${MESSAGE} \e[49m      \e[3${COLOUR}m##########\e[39m"
+}
+
 echo -e "\e[34m############################################################\e[39m"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 echo "Running $0"
@@ -10,7 +31,11 @@ echo "3 is $3"
 echo "@ is $@"
 
 case "$2" in *black*)
-    echo "Black!"
+    pretty_print $1 -1
+    black --version
+    black $TEST --check --diff --line-length $MAX_LINE_LENGTH
+    BLACK_RESULT=$?
+    pretty_print $1 $BLACK_RESULT
     ;;
 esac
 
