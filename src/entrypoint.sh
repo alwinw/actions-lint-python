b12f351640bdc8e2f6b3f8a61f98a2062cee0834
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-26 20:57:48 +1100

    Testing of multiline list
    To be checked in GitHub actions

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,25 +1,8 @@
 #!/bin/sh
-
-# Exit script if any command fails
-# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
-set -eax
-
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-
-
-# Black
-black --version
-
-
-
+echo $1
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
-
-
-# Print version of each 
-isort --version-number
-
-flake8 --version
