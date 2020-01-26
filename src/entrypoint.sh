b2d2c5c2558c610a66935e3050803dd9c701b3b0
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-27 09:57:04 +1100

    Moving entrypoint functions to utils

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,33 +1,12 @@
 #!/bin/sh
 
-function pretty_print() {
-    if [[ $2 -eq -1 ]]
-    then
-        echo -e "\n"
-        local MESSAGE="Running $1 Test"
-        local COLOUR=4
-    elif [[ $2 -eq 0 ]]; then
-        local MESSAGE="$1 Test Passed!"
-        local COLOUR=2
-    elif [[ $2 -eq 1 ]]; then
-        local MESSAGE="$1 Test Failed!"
-        local COLOUR=1
-    else
-        echo -e "\n"
-        local MESSAGE="$1"
-        local COLOUR=4
-    fi
-    echo -e "\e[3${COLOUR}m##########\e[39m     \e[4${COLOUR}m ${MESSAGE} \e[49m      \e[3${COLOUR}m##########\e[39m"
-}
+. ./src/utils.sh
 
 echo -e "\e[34m############################################################\e[39m"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 echo "Running $0"
 echo -e "\e[34m------------------------------------------------------------\e[39m"
 
-echo $PWD
-ls src
-. ./src/utils.sh
 
 PACKAGES=$2
 MAX_LINE_LENGTH=$3
@@ -54,17 +33,6 @@ do
             ;;
     esac
 done
-
-PACKAGE=black
-
-case "$2" in *${PACKAGE}*)
-    pretty_print ${PACKAGE} -1
-    black --version
-    black $TEST --check --diff --line-length $MAX_LINE_LENGTH
-    BLACK_RESULT=$?
-    pretty_print ${PACKAGE} $BLACK_RESULT
-    ;;
-esac
 
 echo -e "\n"
 echo -e "\e[34m------------------------------------------------------------\e[39m"
--- src/utils.sh
+++ src/utils.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
 echo "Sourcing bash utility functions"
+
+function pretty_print() {
+    if [[ $2 -eq -1 ]]
+    then
+        echo -e "\n"
+        local MESSAGE="Running $1 Test"
+        local COLOUR=4
+    elif [[ $2 -eq 0 ]]; then
+        local MESSAGE="$1 Test Passed!"
+        local COLOUR=2
+    elif [[ $2 -eq 1 ]]; then
+        local MESSAGE="$1 Test Failed!"
+        local COLOUR=1
+    else
+        echo -e "\n"
+        local MESSAGE="$1"
+        local COLOUR=4
+    fi
+    echo -e "\e[3${COLOUR}m##########\e[39m     \e[4${COLOUR}m ${MESSAGE} \e[49m      \e[3${COLOUR}m##########\e[39m"
+}
