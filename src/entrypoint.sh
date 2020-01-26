0e33cbb7e73bc075f1c847eb785838337ebbf585
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-26 23:10:46 +1100

    Testing of entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-echo "#################################################"
+echo -e "\e[34m##################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+echo "Running $0"
+echo -e "--------------------------------------------------\e[39m"
 
-echo "# is $#"
-echo "0 is $0"
 echo "1 is $1"
 echo "2 is $2"
 echo "3 is $3"
 echo "@ is $@"
 
-if [[ $3 == *"black" ]]; then
+if [[ $2 == *"black" ]]; then
     echo "Black!"
 fi
 
-echo "#################################################"
+echo -e "\e[34m##################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+echo -e "--------------------------------------------------\e[39m"
