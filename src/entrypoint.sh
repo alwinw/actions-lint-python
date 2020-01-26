4764919e6dca23182c40b6692a2fc10230ebdd14
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-26 21:04:21 +1100

    entrypoint.sh testing
    Checking arguments passed to entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,10 @@
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
+echo $#
+echo $0
 echo $1
+echo $2
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
