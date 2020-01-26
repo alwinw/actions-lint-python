235e8ee9b3d60031f369a96ee5a7757cb09cf753
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-27 10:55:26 +1100

    Added all supported packages

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,10 +3,12 @@
 
 header_print
 
+# INPUT ARSG
 DIRS=$1
 PACKAGES=$2
 MAX_LINE_LENGTH=$3
 
+# RUN THROUGH EACH PACKAGE TEST
 SUPPORTED_PACKAGES="black pycodestyle flake8 pylint isort mypy"
 GLOBAL_RESULT=0
 LOCAL_RESULT=0
@@ -19,23 +21,48 @@ do
                 black)
                     black --version
                     black $DIRS --check --diff --line-length $MAX_LINE_LENGTH
-                    LOCAL_RESULT=$?
+                    ;;
+                pycodestyle)
+                    pycodestyle --version
+                    pycodestyle $DIRS --statistics --max-line-length $MAX_LINE_LENGTH
                     ;;
                 flake8)
                     flake8 --version
+                    flake8 $DIRS --max-line-length=$MAX_LINE_LENGTH
                     ;;
+                pylint)
+                    pylint --version
+                    pylint $DIRS
+                    ;;
+                isort)
+                    isort --version-number
+                    isort $DIRS --recursive --check-only --diff
+                    ;;
+                mypy)
+                    mypy --version
+                    mypy $DIRS --show-column-numbers
             esac
+            LOCAL_RESULT=$?
             GLOBAL_RESULT=$((GLOBAL_RESULT + LOCAL_RESULT))
             pretty_print $PACKAGE $LOCAL_RESULT
             ;;
         *)
             echo "$PACKAGE not supported!!"
             pretty_print
-            # exit 1
+            exit 1
             ;;
     esac
 done
 
 echo $GLOBAL_RESULT
+
+if [ $GLOBAL_RESULT -eq 0 ]
+then
+    exit 0
+else
+    echo GIT DIFF REQUIRED
+fi
+
+# PERFOM GIT DIFF IF REQUIRED
 
 footer_print
