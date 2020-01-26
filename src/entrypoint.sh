1cb515a28251beaa98588b3055d086330a6675fb
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-26 21:10:52 +1100

    Additional testing
    Check print output on Actions

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,10 +2,12 @@
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-echo $#
-echo $0
-echo $1
-echo $2
+echo "# is $#"
+echo "0 is $0"
+echo "1 is $1"
+echo "2 is $2"
+echo "3 is $3"
+echo "@ is $@"
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
