c94bbe75a95ffffadd279bcb62add2ca8b282be8
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-01-27 11:01:53 +1100

    Tidied up entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -15,29 +15,29 @@ LOCAL_RESULT=0
 for PACKAGE in $PACKAGES
 do
     case $SUPPORTED_PACKAGES in
-        *${PACKAGE}*) 
+        *${PACKAGE}*)
             pretty_print $PACKAGE -1
             case $PACKAGE in
                 black)
                     black --version
                     black $DIRS --check --diff --line-length $MAX_LINE_LENGTH
-                    ;;
+                ;;
                 pycodestyle)
                     pycodestyle --version
                     pycodestyle $DIRS --statistics --max-line-length $MAX_LINE_LENGTH
-                    ;;
+                ;;
                 flake8)
                     flake8 --version
                     flake8 $DIRS --max-line-length=$MAX_LINE_LENGTH
-                    ;;
+                ;;
                 pylint)
                     pylint --version
                     pylint $DIRS
-                    ;;
+                ;;
                 isort)
                     isort --version-number
                     isort $DIRS --recursive --check-only --diff
-                    ;;
+                ;;
                 mypy)
                     mypy --version
                     mypy $DIRS --show-column-numbers
@@ -45,24 +45,21 @@ do
             LOCAL_RESULT=$?
             GLOBAL_RESULT=$((GLOBAL_RESULT + LOCAL_RESULT))
             pretty_print $PACKAGE $LOCAL_RESULT
-            ;;
+        ;;
         *)
             echo "$PACKAGE not supported!!"
             pretty_print
             exit 1
-            ;;
+        ;;
     esac
 done
 
-echo $GLOBAL_RESULT
-
 if [ $GLOBAL_RESULT -eq 0 ]
 then
+    footer_print
     exit 0
 else
-    echo GIT DIFF REQUIRED
+    git --no-pager diff
+    footer_print
+    exit 1
 fi
-
-# PERFOM GIT DIFF IF REQUIRED
-
-footer_print
